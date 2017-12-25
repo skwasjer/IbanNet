5c14a4eb902ea30ede77fd3945f104b2d58b79b0
skwas <skwas@skwas.net>
2017-12-25 21:26:35 +0100

    (add) added AppVeyor build number to version

--- ci/set_version_from_git.ps1
+++ ci/set_version_from_git.ps1
@@ -7,16 +7,18 @@ if ($versions)
 {
     # Build new version string
     $newVersion = "$($versions.Version.Major).$($versions.Version.Minor).$($versions.Version.Patch)"
+    $newAppVeyorVersion = "$newVersion.$env:APPVEYOR_BUILD_NUMBER"
     if ($versions.Version.Suffix)
     {
         $newVersion += "-" + $versions.Version.Suffix
+        $newAppVeyorVersion += "-" + $versions.Version.Suffix
     }
 
     Write-Host "AppVeyor build $env:APPVEYOR_BUILD_NUMBER will be using version v$newVersion"
 
     Try
     {
-        Update-AppveyorBuild -Version $newVersion
+        Update-AppveyorBuild -Version $newAppVeyorVersion
     }
     Catch
     {
