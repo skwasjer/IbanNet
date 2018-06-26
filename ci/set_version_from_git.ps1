e9c2b6d09082b78272198dfe02cc00a9d8ece786
skwas <skwas@skwas.net>
2018-06-26 08:20:48 +0200

    Fix incorrect package version (should not include revision)

--- ci/set_version_from_git.ps1
+++ ci/set_version_from_git.ps1
@@ -7,22 +7,25 @@ if ($versions)
 {
     # Build new version string
     $newVersion = "$($versions.InfoVersion.Major).$($versions.InfoVersion.Minor).$($versions.InfoVersion.Patch)"
+
+	$package_version = $newVersion
 	$assembly_version = "$newVersion.0"
 	$fullVersion = "$newVersion.$env:APPVEYOR_BUILD_NUMBER"
     $semVer1 = $fullVersion
 
     if ($versions.InfoVersion.Suffix)
     {
+		$package_version += "-" + $versions.InfoVersion.Suffix
         $semVer1 += "-" + $versions.InfoVersion.Suffix
     }
 
 	$informational_version = $semVer1
 
-    Write-Host "Build v$fullVersion will be building package version v$semVer1"
+    Write-Host "Build v$semVer1 will be building package version v$package_version"
 
     Try
     {
-        Set-AppveyorBuildVariable "package_version" $semVer1		
+        Set-AppveyorBuildVariable "package_version" $package_version		
         Set-AppveyorBuildVariable "assembly_version" $assembly_version
         Set-AppveyorBuildVariable "file_version" $fullVersion
         Set-AppveyorBuildVariable "informational_version" $semVer1
