ffbb40846809a640afdee35d48479ad9796db52d
skwas <skwas@skwas.net>
2019-02-14 21:45:36 +0100

    Simplify structure parsing

--- src/IbanNet/Validation/SwiftStructureValidationFactory.cs
+++ src/IbanNet/Validation/SwiftStructureValidationFactory.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -11,26 +10,13 @@ namespace IbanNet.Validation
 	/// </summary>
 	internal class SwiftStructureValidationFactory : IStructureValidationFactory
 	{
-		private class Markers : IEnumerable<char>
+		private static readonly IDictionary<char, string> RegexMap = new Dictionary<char, string>
 		{
-			public const char Digits = 'n';
-			public const char UppercaseLetters = 'a';
-			public const char Alphanumeric = 'c';
-			public const char Space = 'e';
-
-			public IEnumerator<char> GetEnumerator()
-			{
-				yield return Digits;
-				yield return UppercaseLetters;
-				yield return Alphanumeric;
-				yield return Space;
-			}
-
-			IEnumerator IEnumerable.GetEnumerator()
-			{
-				return GetEnumerator();
-			}
-		}
+			{ 'n', "[0-9]" },
+			{ 'a', "[A-Z]" },
+			{ 'c', "[A-Za-z0-9]" },
+			{ 'e', "[ ]" }
+		};
 
 		/// <inheritdoc />
 		// ReSharper disable once InconsistentNaming
@@ -44,11 +30,11 @@ namespace IbanNet.Validation
 
 		private string BuildStructureRegexPattern(string structure)
 		{
-			IEnumerable<string> pattern = structure
+			IEnumerable<string> structureSegments = structure
 				.Substring(2)
-				.PartitionOn(new Markers().ToArray())
-				.Select(GetTestPart);
-			return $"^{structure.Substring(0, 2)}{string.Join("", pattern)}$";
+				.PartitionOn(RegexMap.Keys.ToArray())
+				.Select(GetRegexPattern);
+			return $"^{structure.Substring(0, 2)}{string.Join("", structureSegments)}$";
 		}
 
 		/// <remarks>
@@ -57,35 +43,21 @@ namespace IbanNet.Validation
 		/// ! = fixed
 		/// marker
 		/// </remarks>
-		private string GetTestPart(string pattern)
+		private string GetRegexPattern(string structureSegment)
 		{
-			string testPattern;
-			char marker = pattern[pattern.Length - 1];
-			switch (marker)
+			char marker = structureSegment[structureSegment.Length - 1];
+			if (!RegexMap.TryGetValue(marker, out string regexPattern))
 			{
-				case Markers.Digits:
-					testPattern = "0-9";
-					break;
-				case Markers.UppercaseLetters:
-					testPattern = "A-Z";
-					break;
-				case Markers.Alphanumeric:
-					testPattern = "A-Za-z0-9";
-					break;
-				case Markers.Space:
-					testPattern = " ";
-					break;
-				default:
-					throw new ArgumentException($"The pattern {pattern} is invalid.", nameof(pattern));
+				throw new ArgumentException($"The structure segment '{structureSegment}' is invalid.", nameof(structureSegment));
 			}
 
-			string lengthDescriptor = pattern.Substring(0, pattern.Length - 1);
+			string lengthDescriptor = structureSegment.Substring(0, structureSegment.Length - 1);
 			bool isFixedLength = lengthDescriptor.EndsWith("!");
 			int occurrences = int.Parse(
 				lengthDescriptor.Substring(0, lengthDescriptor.Length - Convert.ToByte(isFixedLength))
 			);
 
-			return $"([{testPattern}]{{{occurrences}}})";
+			return $"({regexPattern}{{{occurrences}}})";
 		}
 	}
 }
