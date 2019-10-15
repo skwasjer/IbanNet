51e99baa99f5b032a5f9f68585f093a634651a38
skwas <skwas@skwas.net>
2019-10-16 00:10:30 +0200

    Partial revert, the custom upload of test reports was needed because NUnit reports are not supported by AppVeyor.
    This partially reverts commit 099472f2376fc87956dfe105ac49060f849da837.

---
+++ ci/upload-testresults.ps1
@@ -0,0 +1,11 @@
+﻿$wc = New-Object 'System.Net.WebClient'
+
+$files = Get-ChildItem test-result*.xml -Recurse
+
+$uploadUri = "https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)"
+Write-Host "Uploading test results to $($uploadUri)"
+foreach ($file in $files)
+{
+    Write-Host "$($file)" -ForegroundColor Gray
+    $wc.UploadFile($uploadUri, $file)
+}
