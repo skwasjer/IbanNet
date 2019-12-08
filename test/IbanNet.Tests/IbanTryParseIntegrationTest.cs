d33387cad30bb6341561689932a18dbb52e08814
skwas <skwas@skwas.net>
2019-12-08 15:41:14 +0100

    Simplify to only 2 test cases

--- test/IbanNet.Tests/IbanTryParseIntegrationTest.cs
+++ test/IbanNet.Tests/IbanTryParseIntegrationTest.cs
@@ -12,16 +12,17 @@ namespace IbanNet
 			Iban.Validator = new IbanValidator();
 		}
 
-		[TestCaseSource(typeof(IbanTestCaseData), nameof(IbanTestCaseData.GetInvalidIbanPerCountry))]
-		public void Given_a_model_with_invalid_iban_when_parsing_should_give_same_result_as_validator(string countryCode, string attemptedIbanValue)
+		[TestCase("AD1200012030200359100100")] // Valid
+		[TestCase("AD12000120359100100")] // Invalid
+		public void Given_a_model_with_iban_when_parsing_should_give_same_result_as_validator(string attemptedIbanValue)
 		{
 			// Act
 			ValidationResult validatorResult = Iban.Validator.Validate(attemptedIbanValue);
-			bool tryParseResult = Iban.TryParse(attemptedIbanValue, out Iban iban, out ValidationResult validationResult, out Exception _);
+			bool tryParseResult = Iban.TryParse(attemptedIbanValue, out Iban iban, out ValidationResult tryParseValidationResult, out Exception _);
 
 			// Assert
 			validatorResult.IsValid.Should().Be(tryParseResult);
-			validatorResult.Should().BeEquivalentTo(validationResult);
+			validatorResult.Should().BeEquivalentTo(tryParseValidationResult);
 			if (tryParseResult)
 			{
 				iban.Should().NotBeNull();
