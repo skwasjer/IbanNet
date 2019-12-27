6c0eefe9e8f961870266bea63b3b190d08114da2
skwas <skwas@skwas.net>
2019-12-27 18:12:47 +0100

    Assert validation method returns correct rules in order

---
+++ test/IbanNet.Tests/Validation/Methods/StrictValidationTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using FluentAssertions;
+using IbanNet.Registry;
+using IbanNet.Validation.Rules;
+using NUnit.Framework;
+
+namespace IbanNet.Validation.Methods
+{
+	[TestFixture]
+	public class StrictValidationTests
+	{
+		[Test]
+		public void When_getting_rules_it_should_return_all_strict_rules()
+		{
+			var sut = new StrictValidation();
+
+			// Act
+			IEnumerable<IIbanValidationRule> rules = sut.GetRules(
+				new ReadOnlyDictionary<string, CountryInfo>(new Dictionary<string, CountryInfo>())
+			);
+
+			// Assert
+			rules.Select(r => r.GetType())
+				.Should()
+				.BeEquivalentTo(
+					new[] {
+					typeof(NotEmptyRule),
+					typeof(HasCountryCodeRule),
+					typeof(NoIllegalCharactersRule),
+					typeof(HasIbanChecksumRule),
+					typeof(IsValidCountryCodeRule),
+					typeof(IsValidLengthRule),
+					typeof(IsMatchingStructureRule),
+					typeof(Mod97Rule)
+					},
+					options => options.WithStrictOrdering()
+				);
+		}
+	}
+}
