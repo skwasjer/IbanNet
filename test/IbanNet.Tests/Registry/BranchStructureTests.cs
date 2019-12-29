f343dea276de91e6040abcf6ef6a2cdc72519a9c
skwas <skwas@skwas.net>
2019-12-29 18:07:07 +0100

    Cover ctors

---
+++ test/IbanNet.Tests/Registry/BranchStructureTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace IbanNet.Registry
+{
+	[TestFixture]
+	public class BranchStructureTests
+	{
+		[Test]
+		public void When_creating_with_null_structure_it_should_throw()
+		{
+			string structure = null;
+
+			// Act
+			// ReSharper disable once ExpressionIsAlwaysNull
+			// ReSharper disable once ObjectCreationAsStatement
+			Action act = () => new BranchStructure(structure);
+
+			// Assert
+			act.Should()
+				.Throw<ArgumentNullException>()
+				.Which.ParamName.Should()
+				.Be(nameof(structure));
+		}
+	}
+}
