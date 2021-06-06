a12732362a5d71afe90daf6a9a52d147ae916f07
skwasjer <skwas@skwas.net>
2021-06-06 22:37:42 +0200

    chore: disable parallelization for some tests

--- test/IbanNet.DependencyInjection.Autofac.Tests/Specs/ShouldPreserveStaticValidatorSpec.cs
+++ test/IbanNet.DependencyInjection.Autofac.Tests/Specs/ShouldPreserveStaticValidatorSpec.cs
@@ -1,10 +1,7 @@
 ﻿using IbanNet.DependencyInjection.Autofac.Fixtures;
-using TestHelpers;
-using Xunit;
 
 namespace IbanNet.DependencyInjection.Autofac.Specs
 {
-    [Collection(nameof(SetsStaticValidator))]
     public class ShouldPreserveStaticValidatorSpec : TestHelpers.Specs.ShouldPreserveStaticValidatorSpec
     {
         public ShouldPreserveStaticValidatorSpec() : base(new AutofacDependencyInjectionFixture(true))
--- test/IbanNet.DependencyInjection.ServiceProvider.Tests/Specs/ShouldPreserveStaticValidatorSpec.cs
+++ test/IbanNet.DependencyInjection.ServiceProvider.Tests/Specs/ShouldPreserveStaticValidatorSpec.cs
@@ -1,10 +1,7 @@
 ﻿using IbanNet.DependencyInjection.ServiceProvider.Fixtures;
-using TestHelpers;
-using Xunit;
 
 namespace IbanNet.DependencyInjection.ServiceProvider.Specs
 {
-    [Collection(nameof(SetsStaticValidator))]
     public class ShouldPreserveStaticValidatorSpec : TestHelpers.Specs.ShouldPreserveStaticValidatorSpec
     {
         public ShouldPreserveStaticValidatorSpec() : base(new ServiceProviderDependencyInjectionFixture(true))
