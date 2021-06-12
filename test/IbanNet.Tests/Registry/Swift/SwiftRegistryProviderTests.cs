da02421cc38885788a79c7e83f2d7d272e2f0e05
skwasjer <skwas@skwas.net>
2021-06-12 14:09:07 +0200

    Make method abstract, and fix pattern validator when pattern is shorter than max length

---
+++ test/IbanNet.Tests/Registry/Swift/SwiftRegistryProviderTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace IbanNet.Registry.Swift
+{
+    public class SwiftRegistryProviderTests : BaseRegistryProviderSpec<SwiftRegistryProvider>
+    {
+        public SwiftRegistryProviderTests() : base(78)
+        {
+        }
+
+        protected override Task<SwiftRegistryProvider> CreateSubjectAsync()
+        {
+            return Task.FromResult(new SwiftRegistryProvider());
+        }
+    }
+}
