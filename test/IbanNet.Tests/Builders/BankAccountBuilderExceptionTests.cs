fa8f5a8dc8aa90d08bf91bc2aec458a5ab59a614
skwasjer <skwas@skwas.net>
2021-06-19 19:32:57 +0200

    Fix issue referencing wrong version causing coverage to be measured incorrectly. Also improve coverage for 2 exceptions.

---
+++ test/IbanNet.Tests/Builders/BankAccountBuilderExceptionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using Newtonsoft.Json;
+using TestHelpers.Specs;
+using Xunit;
+
+namespace IbanNet.Builders
+{
+    public class BankAccountBuilderExceptionTests : BaseExceptionTests<BankAccountBuilderException>
+    {
+#if NETFRAMEWORK || NETCOREAPP3_0 || NETCOREAPP3_1 || NET5_0
+        [Fact]
+        public void Given_exception_with_parameters_it_should_serialize_and_deserialize()
+        {
+            var exception = new BankAccountBuilderException("some error");
+
+            string jsonWithException = JsonConvert.SerializeObject(exception);
+
+            // Act
+            Exception actual = JsonConvert.DeserializeObject<BankAccountBuilderException>(jsonWithException);
+
+            // Assert
+            actual.Should().BeEquivalentTo(exception);
+        }
+#endif
+    }
+}
