5ab848a5ba642e14071fcae542e079478c5024c0
skwasjer <skwas@skwas.net>
2021-06-19 19:19:30 +0200

    Use .NET API to init array.

--- test/IbanNet.Tests/Extensions/ArrayExtensionsTests.cs
+++ test/IbanNet.Tests/Extensions/ArrayExtensionsTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Xunit;
 
 namespace IbanNet.Extensions
@@ -8,19 +7,6 @@ namespace IbanNet.Extensions
     {
         public class FillTests : ArrayExtensionsTests
         {
-            [Fact]
-            public void Given_null_array_when_filling_it_should_not_throw_and_return_null()
-            {
-                int[] array = null;
-
-                // Act
-                // ReSharper disable once AssignNullToNotNullAttribute
-                Func<int[]> act = () => array.Fill(1);
-
-                // Assert
-                act.Should().NotThrow().Which.Should().BeNull();
-            }
-
             [Fact]
             public void Given_array_when_filling_it_should_not_throw_and_return_null()
             {
