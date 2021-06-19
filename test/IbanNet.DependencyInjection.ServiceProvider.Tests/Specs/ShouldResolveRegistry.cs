289dfdc820985ede1c7975644f6959cdd387df63
skwasjer <skwas@skwas.net>
2021-06-19 18:31:43 +0200

    Add IIbanRegistry as resolvable service from DI

---
+++ test/IbanNet.DependencyInjection.ServiceProvider.Tests/Specs/ShouldResolveRegistry.cs
@@ -0,0 +1,11 @@
+﻿using IbanNet.DependencyInjection.ServiceProvider.Fixtures;
+
+namespace IbanNet.DependencyInjection.ServiceProvider.Specs
+{
+    public class ShouldResolveRegistry : TestHelpers.Specs.ShouldResolveRegistry
+    {
+        public ShouldResolveRegistry() : base(new ServiceProviderDependencyInjectionFixture(true))
+        {
+        }
+    }
+}
