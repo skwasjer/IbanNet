503465c0634254ceb2967ce70b8bb37b85167cf1
skwasjer <skwas@skwas.net>
2021-09-26 12:36:45 +0200

    fix(net6): CS8765: Nullability of type of parameter 'xxxxx' doesn't match overridden member

--- src/IbanNet/TypeConverters/IbanTypeConverter.cs
+++ src/IbanNet/TypeConverters/IbanTypeConverter.cs
@@ -18,13 +18,13 @@ namespace IbanNet.TypeConverters
         }
 
         /// <inheritdoc />
-        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
         {
             return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
         }
 
         /// <inheritdoc />
-        public override object? ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
         {
             switch (value)
             {
