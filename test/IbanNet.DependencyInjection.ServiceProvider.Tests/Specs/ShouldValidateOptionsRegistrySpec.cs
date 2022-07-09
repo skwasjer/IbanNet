478bf69f7a240ea875e5d13101580061f9c3e147
skwasjer <skwas@skwas.net>
2022-07-09 21:56:34 +0200

    test: reenable tests for supported target frameworks

--- test/IbanNet.DependencyInjection.ServiceProvider.Tests/Specs/ShouldValidateOptionsRegistrySpec.cs
+++ test/IbanNet.DependencyInjection.ServiceProvider.Tests/Specs/ShouldValidateOptionsRegistrySpec.cs
@@ -5,34 +5,38 @@ using TestHelpers.Specs;
 
 namespace IbanNet.DependencyInjection.ServiceProvider.Specs
 {
-	public class ShouldValidateOptionsRegistrySpec : DiSpec
-	{
-		public ShouldValidateOptionsRegistrySpec() : base(new ServiceProviderDependencyInjectionFixture(true))
-		{
-		}
+    public class ShouldValidateOptionsRegistrySpec : DiSpec
+    {
+        public ShouldValidateOptionsRegistrySpec() : base(new ServiceProviderDependencyInjectionFixture(true))
+        {
+        }
 
-		protected override void Given()
-		{
-			Fixture.Configure(builder =>
-			{
+        protected override void Given()
+        {
+            Fixture.Configure(builder =>
+            {
                 // ReSharper disable once AssignNullToNotNullAttribute
                 IIbanNetOptionsBuilder returnedBuilder = builder.Configure(options => options.Registry = null);
 
-				returnedBuilder.Should().BeSameAs(builder);
-			});
-		}
+                returnedBuilder.Should().BeSameAs(builder);
+            });
+        }
 
-		[Fact(Skip = "Currently targeting .NET Standard 2.0 which does not support options validation.")]
-		public void Given_registry_is_null_when_getting_options_it_should_ensure_not_null_through_validation()
-		{
-			Func<IbanValidatorOptions> act = () => Subject.GetService<IOptions<IbanValidatorOptions>>()?.Value;
+#if NET5_0_OR_GREATER
+        [Fact]
+#else
+        [Fact(Skip = "Currently targeting .NET Standard 2.0 which does not support options validation.")]
+#endif
+        public void Given_registry_is_null_when_getting_options_it_should_ensure_not_null_through_validation()
+        {
+            Func<IbanValidatorOptions> act = () => Subject.GetService<IOptions<IbanValidatorOptions>>()?.Value;
 
-			// Assert
-			act.Should()
-				.Throw<OptionsValidationException>()
-				.Which.Message.Should()
-				.Be("The 'Registry' is required.");
-		}
-	}
+            // Assert
+            act.Should()
+                .Throw<OptionsValidationException>()
+                .Which.Message.Should()
+                .Be("The 'Registry' is required.");
+        }
+    }
 }
 #endif
