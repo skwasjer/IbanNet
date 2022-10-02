6cb492e8af25d583a9e5c9193934619bd59145a8
skwasjer <skwas@skwas.net>
2022-10-02 05:17:11 +0200

    fix: code smell CA1018: Mark attributes with AttributeUsageAttribute.

--- test/TestHelpers/AssertionMethodAttribute.cs
+++ test/TestHelpers/AssertionMethodAttribute.cs
@@ -3,4 +3,5 @@
 /// <summary>
 /// Marks a method as an assertion method as a hint for SonarQube: S2699
 /// </summary>
+[AttributeUsage(AttributeTargets.Method)]
 public class AssertionMethodAttribute : Attribute { }
