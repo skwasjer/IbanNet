ea7f247c4fd3fd081280c5e84ec44145d4f862b5
skwasjer <skwas@skwas.net>
2022-10-04 19:09:56 +0200

    chore: add IsExternalInit

--- src/IbanNet/Properties/AssemblyInfoAttributes.cs
+++ src/IbanNet/Properties/AssemblyInfoAttributes.cs
@@ -8,12 +8,3 @@ using System.Runtime.CompilerServices;
 #endif
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: CLSCompliant(true)]
-
-#if !NET6_0_OR_GREATER
-// https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
-// ReSharper disable once CheckNamespace
-namespace System.Runtime.CompilerServices
-{
-      internal static class IsExternalInit {}
-}
-#endif
