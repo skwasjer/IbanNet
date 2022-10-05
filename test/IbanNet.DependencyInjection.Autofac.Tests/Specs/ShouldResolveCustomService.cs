bb4dc2b9377a93eeba059b26de9146b1e1e2c97b
skwasjer <skwas@skwas.net>
2022-10-05 23:11:55 +0200

    fix(autofac): fix registration bug where if custom registry was registered, it would still register IbanNet's

---
+++ test/IbanNet.DependencyInjection.Autofac.Tests/Specs/ShouldResolveCustomService.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using IbanNet.DependencyInjection.Autofac.Fixtures;
+using IbanNet.Registry;
+
+namespace IbanNet.DependencyInjection.Autofac.Specs
+{
+    public class ShouldResolveCustomService : TestHelpers.Specs.ShouldResolveCustomService
+    {
+        public ShouldResolveCustomService() : base(new CustomServicesDependencyInjectionFixture())
+        {
+        }
+
+        private class CustomServicesDependencyInjectionFixture
+            : AutofacDependencyInjectionFixture
+        {
+            public CustomServicesDependencyInjectionFixture() : base(false)
+            {
+            }
+
+            protected override void Configure(ContainerBuilder containerBuilder, Action<IIbanNetOptionsBuilder> configurer)
+            {
+                containerBuilder.RegisterInstance(Mock.Of<IIbanRegistry>());
+                containerBuilder.RegisterInstance(Mock.Of<IIbanParser>());
+                containerBuilder.RegisterInstance(Mock.Of<IIbanGenerator>());
+                containerBuilder.RegisterInstance(Mock.Of<IIbanValidator>());
+
+                base.Configure(containerBuilder, configurer);
+            }
+        }
+    }
+}
---
+++ test/IbanNet.DependencyInjection.ServiceProvider.Tests/Specs/ShouldResolveCustomService.cs
@@ -0,0 +1,31 @@
+﻿using IbanNet.DependencyInjection.ServiceProvider.Fixtures;
+using IbanNet.Registry;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IbanNet.DependencyInjection.ServiceProvider.Specs
+{
+    public class ShouldResolveCustomService : TestHelpers.Specs.ShouldResolveCustomService
+    {
+        public ShouldResolveCustomService() : base(new CustomServicesDependencyInjectionFixture())
+        {
+        }
+
+        private class CustomServicesDependencyInjectionFixture
+            : ServiceProviderDependencyInjectionFixture
+        {
+            public CustomServicesDependencyInjectionFixture() : base(false)
+            {
+            }
+
+            protected override void Configure(IServiceCollection services, Action<IIbanNetOptionsBuilder> configurer)
+            {
+                services.AddSingleton(Mock.Of<IIbanRegistry>());
+                services.AddSingleton(Mock.Of<IIbanParser>());
+                services.AddSingleton(Mock.Of<IIbanGenerator>());
+                services.AddSingleton(Mock.Of<IIbanValidator>());
+
+                base.Configure(services, configurer);
+            }
+        }
+    }
+}
