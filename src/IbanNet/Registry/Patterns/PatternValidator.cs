dc2417bdb0d77f900556a35ab8d507647670a823
skwasjer <skwas@skwas.net>
2022-10-05 20:57:58 +0200

    refactor: simplify PatternValidator

--- src/IbanNet/Registry/Patterns/PatternValidator.cs
+++ src/IbanNet/Registry/Patterns/PatternValidator.cs
@@ -3,24 +3,22 @@
     internal class PatternValidator
     {
         private readonly IReadOnlyList<PatternToken> _tokens;
-#if USE_SPANS
         private readonly bool _isFixedLength;
-#else
-        private readonly Func<string, bool> _validationFunc;
-#endif
 
         public PatternValidator(Pattern pattern)
         {
             _tokens = pattern.Tokens;
-#if USE_SPANS
             _isFixedLength = pattern.IsFixedLength;
-#else
-            _validationFunc = GetValidationMethod(pattern);
-#endif
         }
 
+        public bool Validate
+        (
 #if USE_SPANS
-        public bool Validate(ReadOnlySpan<char> value)
+            ReadOnlySpan<char> value
+#else
+            string value
+#endif
+        )
         {
             // If no tokens, always fail.
             if (_tokens.Count == 0)
@@ -34,115 +32,22 @@
                 : ValidateNonFixedLength(value);
         }
 
+#if USE_SPANS
         private bool ValidateFixedLength(ReadOnlySpan<char> value)
-        {
-            int length = value.Length;
-            int tokenCount = _tokens.Count;
-            int pos = 0;
-            int segmentIndex = 0;
-            for (; segmentIndex < tokenCount; segmentIndex++)
-            {
-                PatternToken expectedToken = _tokens[segmentIndex];
-                int maxLength = expectedToken.MaxLength;
-
-                if (pos + maxLength > length)
-                {
-                    return false;
-                }
-
-                Func<char, bool> isMatch = expectedToken.IsMatch;
-                for (int occurrence = 0; occurrence < maxLength; occurrence++)
-                {
-                    if (!isMatch(value[pos++]))
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return length == pos && segmentIndex == tokenCount;
-        }
-
-        private bool ValidateNonFixedLength(ReadOnlySpan<char> value)
-        {
-            int pos = 0;
-            int segmentIndex = 0;
-            for (; segmentIndex < _tokens.Count; segmentIndex++)
-            {
-                PatternToken? expectedToken = _tokens[segmentIndex];
-                int count = expectedToken.MaxLength;
-                if (expectedToken.IsFixedLength)
-                {
-                    if (!ProcessFixedLengthTest(expectedToken, value.Slice(pos)))
-                    {
-                        return false;
-                    }
-
-                    pos += expectedToken.MaxLength;
-                }
-                else if (!ProcessNonFixedLengthTest(expectedToken, value.Slice(pos), out count))
-                {
-                    return false;
-                }
-
-                pos += count;
-            }
-
-            return value.Length == pos && segmentIndex == _tokens.Count;
-        }
-
-        private static bool ProcessFixedLengthTest(PatternToken expectedToken, ReadOnlySpan<char> value)
-        {
-            if (expectedToken.MaxLength > value.Length)
-            {
-                return false;
-            }
-
-            for (int occurrence = 0; occurrence < expectedToken.MaxLength; occurrence++)
-            {
-                char c = value[occurrence];
-                if (!expectedToken.IsMatch(c))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        private static bool ProcessNonFixedLengthTest(PatternToken expectedToken, ReadOnlySpan<char> value, out int occurrence)
-        {
-            occurrence = 0;
-            for (; occurrence < expectedToken.MaxLength; occurrence++)
-            {
-                if (occurrence >= value.Length)
-                {
-                    return occurrence >= expectedToken.MinLength && occurrence <= expectedToken.MaxLength;
-                }
-
-                char c = value[occurrence];
-                if (!expectedToken.IsMatch(c))
-                {
-                    return false;
-                }
-            }
-
-            return occurrence >= expectedToken.MinLength && occurrence <= expectedToken.MaxLength;
-        }
-
 #else
-        public bool Validate(string value)
-        {
-            return _validationFunc(value);
-        }
-
         private unsafe bool ValidateFixedLength(string value)
+#endif
         {
             int length = value.Length;
             int tokenCount = _tokens.Count;
             int pos = 0;
             int segmentIndex = 0;
+#if USE_SPANS
+            // ReSharper disable once InlineTemporaryVariable
+            ReadOnlySpan<char> ptr = value;
+#else
             fixed (char* ptr = value)
+#endif
             {
                 for (; segmentIndex < tokenCount; segmentIndex++)
                 {
@@ -168,7 +73,11 @@
             return length == pos && segmentIndex == tokenCount;
         }
 
+#if USE_SPANS
+        private bool ValidateNonFixedLength(ReadOnlySpan<char> value)
+#else
         private bool ValidateNonFixedLength(string value)
+#endif
         {
             int pos = 0;
             int segmentIndex = 0;
@@ -191,7 +100,15 @@
             return value.Length == pos && segmentIndex == _tokens.Count;
         }
 
-        private static bool ProcessFixedLengthTest(PatternToken expectedToken, string value, ref int pos)
+        private static bool ProcessFixedLengthTest(
+            PatternToken expectedToken,
+#if USE_SPANS
+            ReadOnlySpan<char> value,
+#else
+            string value,
+#endif
+            ref int pos
+        )
         {
             if (pos + expectedToken.MaxLength > value.Length)
             {
@@ -212,7 +129,15 @@
             return true;
         }
 
-        private static bool ProcessNonFixedLengthTest(PatternToken expectedToken, string value, ref int pos)
+        private static bool ProcessNonFixedLengthTest(
+            PatternToken expectedToken,
+#if USE_SPANS
+            ReadOnlySpan<char> value,
+#else
+            string value,
+#endif
+            ref int pos
+        )
         {
             int startPos = pos;
             for (int occurrence = 0; occurrence < expectedToken.MaxLength; occurrence++)
@@ -233,23 +158,5 @@
 
             return pos >= startPos + expectedToken.MinLength && pos <= startPos + expectedToken.MaxLength;
         }
-
-        private Func<string, bool> GetValidationMethod(Pattern pattern)
-        {
-            // If no tokens, always fail.
-            if (pattern.Tokens.Count == 0)
-            {
-                return _ => false;
-            }
-
-            // Short-circuit, if all tests are fixed length, use faster validation.
-            if (pattern.IsFixedLength)
-            {
-                return ValidateFixedLength;
-            }
-
-            return ValidateNonFixedLength;
-        }
-#endif
     }
 }
