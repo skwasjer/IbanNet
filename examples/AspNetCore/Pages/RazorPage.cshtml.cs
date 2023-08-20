07e619f6afa6d79a21ad6e1e0ca620cde482646a
Martijn Bodeman <11424653+skwasjer@users.noreply.github.com>
2023-08-20 10:25:57 +0200

    docs: update IbanNet dependency in examples [skip ci] (#159)

--- examples/AspNetCore/Pages/RazorPage.cshtml.cs
+++ examples/AspNetCore/Pages/RazorPage.cshtml.cs
@@ -1,37 +1,36 @@
-﻿using ExampleWebApplication.Models;
+﻿using AspNetCoreExample.Models;
 using IbanNet;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ExampleWebApplication.Pages
+namespace AspNetCoreExample.Pages;
+
+/// <summary>
+/// Razor Page example, showing usage of IbanNet.
+/// </summary>
+public class RazorPageModel : PageModel
 {
-	/// <summary>
-	/// Razor Page example, showing usage of IbanNet.
-	/// </summary>
-	public class RazorPageModel : PageModel
-	{
-		private readonly IIbanParser _parser;
+    private readonly IIbanParser _parser;
 
-		public RazorPageModel(IIbanParser parser)
-		{
-			_parser = parser;
-		}
+    public RazorPageModel(IIbanParser parser)
+    {
+        _parser = parser;
+    }
 
-		[BindProperty]
-		public InputModel Model { get; set; }
+    [BindProperty]
+    public InputModel Model { get; set; } = default!;
 
-		public IActionResult OnPostAsync()
-		{
-			if (!ModelState.IsValid)
-			{
-				return Page();
-			}
+    public IActionResult OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
 
-			Iban iban = _parser.Parse(Model.BankAccountNumber);
-			// Do something with model...
-			Model.BankAccountNumber = iban.ToString(IbanFormat.Print);
+        Iban iban = _parser.Parse(Model.BankAccountNumber);
+        // Do something with model...
+        Model.BankAccountNumber = iban.ToString(IbanFormat.Print);
 
-			return Page();
-		}
-	}
+        return Page();
+    }
 }
--- examples/AspNetCore/Startup.cs
+++ examples/AspNetCore/Startup.cs
@@ -2,63 +2,57 @@
 using FluentValidation.AspNetCore;
 #endif
 using IbanNet.DependencyInjection.ServiceProvider;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace ExampleWebApplication
+namespace AspNetCoreExample;
+
+public class Startup
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
 
-		public IConfiguration Configuration { get; }
+    public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			// Register IbanNet.
-			services.AddIbanNet();
-			services
-				.AddRazorPages()
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Register IbanNet.
+        services.AddIbanNet();
+        services
+            .AddRazorPages()
 #if DEBUG_FLUENTVALIDATION
 				.AddFluentValidation(fv => fv
 					.RegisterValidatorsFromAssemblyContaining<Startup>()
                     .DisableDataAnnotationsValidation = true
 				)
 #endif
-				;
+            ;
 
-		}
+    }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Error");
-			}
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+        }
 
-			app.UseStaticFiles();
-			app.UseRouting();
-			app.UseEndpoints(endpoints =>
-			{
-				// For Mvc.
-				endpoints.MapDefaultControllerRoute();
-				// For RazorPages.
-				endpoints.MapRazorPages();
-				// For API controllers using attribute routing.
-				endpoints.MapControllers();
-			});
-		}
-	}
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            // For Mvc.
+            endpoints.MapDefaultControllerRoute();
+            // For RazorPages.
+            endpoints.MapRazorPages();
+            // For API controllers using attribute routing.
+            endpoints.MapControllers();
+        });
+    }
 }
