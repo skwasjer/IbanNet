bf05f9219755759012b8ac7be1b01078aa2fc025
Kemal Taskin <kemal.n.taskin@gmail.com>
2024-06-20 20:36:33 +0200

    SwissIbanExtensions.IsQrIban core functionality is extracted and reused.

--- src/IbanNet/Validation/Rules/QrIbanRule.cs
+++ src/IbanNet/Validation/Rules/QrIbanRule.cs
@@ -1,4 +1,5 @@
-﻿using IbanNet.Extensions;
+﻿using System.Globalization;
+using IbanNet.Registry;
 using IbanNet.Validation.Results;
 
 namespace IbanNet.Validation.Rules;
@@ -11,11 +12,19 @@ internal sealed class QrIbanRule : IIbanValidationRule
     /// <inheritdoc />
     public ValidationRuleResult Validate(ValidationRuleContext context)
     {
-        if (Iban.TryParse(context.Value, out Iban? iban) && SwissIbanExtensions.IsQrIban(iban))
+        IbanCountry? country = context.Country;
+        if (country != null && IsValid(country, context.Value.Substring(country.Bank.Position, country.Bank.Length)))
         {
             return ValidationRuleResult.Success;
         }
 
         return new InvalidQrIbanResult();
+    }
+
+    internal static bool IsValid(IbanCountry country, string iidStr)
+    {
+        return country.TwoLetterISORegionName is "CH" or "LI"
+           && int.TryParse(iidStr, NumberStyles.None, NumberFormatInfo.InvariantInfo, out int iid)
+           && iid is >= 30000 and <= 31999;
     }
 }
--- test/IbanNet.Tests/Validation/Rules/IsValidQrIbanRuleTests.cs
+++ test/IbanNet.Tests/Validation/Rules/IsValidQrIbanRuleTests.cs
@@ -1,4 +1,5 @@
 ﻿using IbanNet.Validation.Results;
+using IbanNet.Registry;
 
 namespace IbanNet.Validation.Rules;
 
@@ -11,8 +12,6 @@ public class IsValidQrIbanRuleTests
         _sut = new QrIbanRule();
     }
 
-    [InlineData("")]
-    [InlineData(" ")]
     [InlineData("DE89370400440532013000")]
     [InlineData("FR1420041010050500013M02606")]
     [InlineData("CH93007620116238529579")]
@@ -22,21 +21,21 @@ public class IsValidQrIbanRuleTests
     [InlineData("DE193A0400440532013000")]
     [InlineData("LI21088100002324013AA")]
     [InlineData("LI7830X74502999200012")]
+    [InlineData("CH44 3199 9123 0008 8901 2")]
     [Theory]
     public void Given_invalid_values_it_should_return_success(string iban)
     {
-        ValidationRuleResult actual = _sut.Validate(new ValidationRuleContext(iban));
+        ValidationRuleResult actual = _sut.Validate(new ValidationRuleContext(iban, IbanRegistry.Default[iban.Substring(0, 2)]));
 
         actual.Should().BeOfType<InvalidQrIbanResult>();
     }
 
     [InlineData("LI7830174502999200012")]
     [InlineData("CH4431999123000889012")]
-    [InlineData("CH44 3199 9123 0008 8901 2")]
     [Theory]
     public void Given_valid_values_it_should_return_success(string iban)
     {
-        ValidationRuleResult actual = _sut.Validate(new ValidationRuleContext(iban));
+        ValidationRuleResult actual = _sut.Validate(new ValidationRuleContext(iban, IbanRegistry.Default[iban.Substring(0, 2)]));
 
         actual.Should().Be(ValidationRuleResult.Success);
     }
