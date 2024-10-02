386c7c5e6f4485d8951a1a2cbf78e5ab75cbe667
skwasjer <11424653+skwasjer@users.noreply.github.com>
2024-10-02 20:03:27 +0200

    test: verify public API

---
+++ test/IbanNet.DependencyInjection.Autofac.Tests/PublicApiTests.cs
@@ -0,0 +1,11 @@
+﻿using TestHelpers.Specs;
+
+namespace IbanNet.DependencyInjection.Autofac;
+
+public sealed class PublicApiTests : PublicApiSpec
+{
+    public PublicApiTests()
+        : base(typeof(AutofacRegistrationExtensions))
+    {
+    }
+}
