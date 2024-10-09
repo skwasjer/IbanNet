6e32d4312b0e08c6c0188e4cff286865eeb4e3f9
skwasjer <11424653+skwasjer@users.noreply.github.com>
2024-10-09 04:43:03 +0200

    fix(CA1062): throw when context is null

--- src/IbanNet/Validation/Rules/QrIbanRule.cs
+++ src/IbanNet/Validation/Rules/QrIbanRule.cs
@@ -12,6 +12,11 @@ public sealed class QrIbanRule : IIbanValidationRule
     /// <inheritdoc />
     public ValidationRuleResult Validate(ValidationRuleContext context)
     {
+        if (context is null)
+        {
+            throw new ArgumentNullException(nameof(context));
+        }
+
         IbanCountry? country = context.Country;
         if (country != null && IsValid(country, context.Value.Substring(country.Bank.Position, country.Bank.Length)))
         {
@@ -24,7 +29,7 @@ public sealed class QrIbanRule : IIbanValidationRule
     internal static bool IsValid(IbanCountry country, string iidStr)
     {
         return country.TwoLetterISORegionName is "CH" or "LI"
-           && int.TryParse(iidStr, NumberStyles.None, NumberFormatInfo.InvariantInfo, out int iid)
-           && iid is >= 30000 and <= 31999;
+         && int.TryParse(iidStr, NumberStyles.None, NumberFormatInfo.InvariantInfo, out int iid)
+         && iid is >= 30000 and <= 31999;
     }
 }
