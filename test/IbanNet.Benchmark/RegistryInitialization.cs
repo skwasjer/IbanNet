e705f882fe49ae9d25807370a01fa7bc3e9b6ab1
Martijn Bodeman <11424653+skwasjer@users.noreply.github.com>
2025-09-15 18:13:42 +0200

    perf: add benchmarks for registry initialization/lookup (#303)
    * perf: add new benchmarks measuring registry initialization and lookup times
    
    * docs: remove Singleton_CacheReuse since it isn't really a good benchmark. It just retests the same IBAN, which skews the outcome. The other benchmarks actually test all countries, with varying patterns/tokens/length.

---
+++ test/IbanNet.Benchmark/RegistryInitialization.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using IbanNet.Registry;
+using IbanNet.Registry.Swift;
+using IbanNet.Registry.Wikipedia;
+
+namespace IbanNet.Benchmark;
+
+[MarkdownExporterAttribute.GitHub]
+[Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+[MemoryDiagnoser]
+public class RegistryInitialization
+{
+    public static IEnumerable<object> GetProviders()
+    {
+        yield return new Args(new SwiftRegistryProvider());
+        yield return new Args(new WikipediaRegistryProvider());
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(GetProviders))]
+    public void Initialize(Args args)
+    {
+        _ = new IbanRegistry { Providers = { args.Provider } };
+    }
+
+    public sealed class Args(IIbanRegistryProvider provider)
+    {
+        public IIbanRegistryProvider Provider { get; } = provider;
+
+        public override string ToString()
+        {
+            return Provider.GetType().Name.Replace("RegistryProvider", string.Empty);
+        }
+    }
+}
