6b95a13e95b2fa203eaa50fdb975e6e9dcffb987
skwasjer <11424653+skwasjer@users.noreply.github.com>
2025-11-05 14:58:17 +0100

    chore!: remove obsolete enum member AsciiCategory.Other

--- src/IbanNet/Registry/Patterns/AsciiCategory.cs
+++ src/IbanNet/Registry/Patterns/AsciiCategory.cs
@@ -4,25 +4,12 @@
 /// Defines the ASCII category of a character.
 /// </summary>
 [Flags]
-#pragma warning disable S2342 // Enumeration types should comply with a naming convention - justification: breaking change
-#pragma warning disable CA1008
 public enum AsciiCategory
-#pragma warning restore CA1008
-#pragma warning restore S2342
 {
     /// <summary>
     /// No ASCII category.
     /// </summary>
     None = 0,
-
-    /// <summary>
-    /// No ASCII category.
-    /// </summary>
-    [Obsolete("Use None instead.", true)]
-#pragma warning disable CA1069
-    // ReSharper disable once UnusedMember.Global
-    Other = 0,
-#pragma warning restore CA1069
 
     /// <summary>
     /// The space character.
